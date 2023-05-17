52dc7870370967c83a03240b0193037d7a99c792
HasibulHasan <hasibulhasan372@gmail.com>
2023-05-18 04:05:05 +0600

    craete signup page and fixed the conditional header of my-toys, user pro pic, add toys and login, logout btn

--- src/pages/login/SignUp/SignUp.jsx
+++ src/pages/login/SignUp/SignUp.jsx
@@ -1,18 +1,57 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthProvider/AuthProvider";
+import { updateProfile } from "firebase/auth";
 
 
 const SignUp = () => {
+    const { createUser} = useContext(AuthContext)
+    const handleSignIn = (e) =>{
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, photo, email, password)
+        createUser(email, password)
+        .then(result =>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            updateUserInfo(result.user, name, photo)
+        })
+        .catch(error => console.log(error))
+
+    };
+
+    const updateUserInfo = (currentUser,name, photo) =>{
+         updateProfile(currentUser, {
+            displayName: name, photoURL: photo
+        })
+        .then(()=>{})
+        .then(error => console.log(error))
+    };
+
     return (
         <div className="my-con">
         <div className="lg:w-2/3 lg:p-4 bg-cyan-100 m-auto mt-10 rounded">
             <h1 className="lg:text-5xl lg:font-bold">Sign Up</h1>
-            <form className="mt-8">
+            <form onSubmit={handleSignIn} className="mt-8">
                 <div className="mb-4">
+                <label className="text-lg font-semibold block mb-2">Name</label>
+                <input type="name" name="name" id="name" placeholder="Your name" className="border rounded pl-2 py-2 lg:w-1/2" />
+                </div>
+                <div className="mb-4">
+                <label className="text-lg font-semibold block mb-2">Photo URL</label>
+                <input type="url" name="photo" id="photo" placeholder="Photo URL" className="border rounded pl-2 py-2 lg:w-1/2" />
+                </div>
+                 <div className="mb-4">
                 <label className="text-lg font-semibold block mb-2">Email</label>
-                <input type="email" name="email" id="email" placeholder="your email" className="border rounded pl-2 py-2 lg:w-1/2" />
-                </div> <div className="mb-4">
+                <input type="email" name="email" id="email" placeholder="Your email" className="border rounded pl-2 py-2 lg:w-1/2" />
+                </div>
+                 <div className="mb-4">
                 <label className="text-lg font-semibold block mb-2">Password</label>
-                <input type="password" name="password" id="password" placeholder="your password" className="border rounded pl-2 py-2 lg:w-1/2" />
+                <input type="password" name="password" id="password" placeholder="Your password" className="border rounded pl-2 py-2 lg:w-1/2" />
                 </div>
                 <div>
                    <input type="submit" value="Submit" className="btn btn-bg-color border-rose-400 capitalize text-lg hover:bg-red-600 hover:border-none  " />
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,18 +1,36 @@
+import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider/AuthProvider";
 
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+    
+    const handleLogOut = () =>{
+        logOut();
+    }
+
     return (
         <div className="my-con lg:py-4  ">
-            <div className="flex lg:justify-between lg:items-center">
-                <img src="https://i.ibb.co/JxxscGk/logo.png" alt="logo.png" className="lg:w-20 lg:h-20"  />
+            <div className="flex lg:justify-between  lg:items-center">
+                <img src="https://i.ibb.co/JxxscGk/logo.png" alt="logo.png" className="lg:w-24 lg:h-2w-24" />
 
-                <div className="lg:space-x-12 lg:font-bold lg:text-lg">
+                <div className="lg:space-x-8 lg:font-bold lg:text-lg lg:col-span-3 lg:flex lg:items-center">
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/allToys">All Toys</NavLink>
-                    <NavLink to="/myToys">My Toys</NavLink>
-                    <NavLink to="/addToys">Add Toys</NavLink>
-                    <Link to="/login" className="py-2 px-6 font-semibold border rounded-3xl btn-bg-color text-white">Login</Link>
+                    <div className="lg:space-x-8">
+                        {
+                            user ? <>
+                                <NavLink to="/myToys">My Toys</NavLink>
+                                <NavLink to="/addToys">Add Toys</NavLink>
+                                <img src={user?.photoURL || user?.displayName} alt="userPhoto" className="lg:w-7 lg:h-7 rounded-full inline-block border " />
+                                <button onClick={handleLogOut} className="py-2 px-6 font-semibold border rounded-3xl btn-bg-color text-white">
+                                    Logout
+                                </button>
+                            </> :
+                                <Link to="/login" className="py-2 px-6 font-semibold border rounded-3xl btn-bg-color text-white">Login</Link>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
