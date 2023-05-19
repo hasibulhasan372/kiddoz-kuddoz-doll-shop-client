0d023e55be8811c989f6a6a643445ba4fb8b6e95
HasibulHasan <hasibulhasan372@gmail.com>
2023-05-19 16:49:15 +0600

    Deleted my tos part completed

--- src/card/MyToy/MyToy.jsx
+++ src/card/MyToy/MyToy.jsx
@@ -1,6 +1,6 @@
 
-const MyToy = ({myToy}) => {
-    const { name, img, price, rating, sellerName, quantity, category } = myToy;
+const MyToy = ({myToy, handleDeleteToy}) => {
+    const {_id, name, img, price, rating, sellerName, quantity, category } = myToy;
     return (
         <div className="card w-96 glass " >
             <figure ><img src={img} alt="img" className="lg:w-[250px] lg:h-[250px] object-cover lg:pt-5 text-left" /></figure>
@@ -12,7 +12,10 @@ const MyToy = ({myToy}) => {
                 <p>Quantity: {quantity}</p>
                 <p>Rating: {rating}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary capitalize">View Details</button>
+                    <button className="btn btn-primary capitalize">Update</button>
+
+                    <button onClick={()=>handleDeleteToy(_id)} className="btn btn-warning capitalize">Delete</button>
+                    
                 </div>
             </div>
         </div>
--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -12,16 +12,32 @@ const MyToys = () => {
         fetch(url)
             .then(res => res.json())
             .then(data => setMyToys(data))
-    }, [url])
+    }, [url]);
+
+    const handleDeleteToy = (id) =>{
+        fetch(`https://kiddoz-kuddoz-doll-shop-server.vercel.app/dolls/${id}`,{
+            method:"DELETE"
+        })
+        .then(res => res.json())
+        .then(data => {
+            const remaining = myToys.filter(toy => toy._id !== id)
+            setMyToys(remaining)
+            if(data.deletedCount > 0){
+                alert("Deleted Successfully")
+            }
+        })
+
+    }
 
     return (
         <div className="my-con lg:my-12">
             <h1 className="text-2xk font-bold mb-6">Total My Add Toys: {myToys.length} </h1>
-            <div>
+            <div className="grid grid-cols-3">
                 {
                     myToys.map(myToy => <MyToy
                     key={myToy._id}
                     myToy={myToy}
+                    handleDeleteToy={handleDeleteToy}
                     ></MyToy>)
                 }
             </div>
